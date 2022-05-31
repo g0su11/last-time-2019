132eaa8fe6cefca720eaf5cb00fb597b63dc3068
g0su12 <huy5763@gmail.com>
2022-05-31 15:02:33 +0700

    code

--- src/index.js
+++ src/index.js
@@ -3,15 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Routes, Route, HashRouter } from 'react-router-dom';
+import { About, Information, Home, Prevention } from './@components/main/components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<App/>}>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" exact element={<About/>}/>
+          <Route path="/information" element={<Information/>}/>
+          <Route path="/prevention" element={<Prevention/>}/>
+          {/* <Route
+            path="/"
+            element={
+              <main style={{ padding: "200px", textAlign: "center", fontSize: "1.7rem", color: "white" }}>
+                <p>There's nothing here! Click to another page for more information</p>
+              </main>
+            }
+          /> */}
+        </Route>
+      </Routes>
+    </HashRouter>
+  </React.StrictMode> 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
